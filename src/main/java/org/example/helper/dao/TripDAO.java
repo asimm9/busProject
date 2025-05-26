ce36166197f62b6e1b0fad0f1a604839def8ccb3
Asım <asimisik9@gmail.com>
2025-05-26 21:38:44 +0300

    add trip manager implementation

--- src/main/java/org/example/helper/dao/TripDAO.java
+++ src/main/java/org/example/helper/dao/TripDAO.java
@@ -1,25 +1,99 @@
 package org.example.helper.dao;
 
+import javafx.scene.control.DatePicker;
 import org.example.helper.DatabaseConnector;
+import org.example.models.Bus;
+import org.example.models.Seat;
 import org.example.models.Trip;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TripDAO {
     public boolean insertTrip(Trip trip) {
-        String sql = "INSERT INTO trips(trip_id, origin, destination, departure_time, bus_id) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO trips(trip_id, origin, destination, departure_time, time, bus_id) VALUES (?, ?, ?, ?, ?, ?)";
         try (Connection conn = DatabaseConnector.connect(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setInt(1, trip.getTripID());
+            stmt.setString(1, trip.getTripID());
             stmt.setString(2, trip.getOrigin());
             stmt.setString(3, trip.getDestination());
             stmt.setString(4, trip.getDepartureTime().toString());
-            stmt.setInt(5, trip.getBus().getBusID());
+            stmt.setString(5, trip.getTime().toString());
+            stmt.setInt(6, trip.getBus().getBusID());
             return stmt.executeUpdate() > 0;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public boolean deleteTrip(Trip trip) {
+        String sql = "DELETE FROM trips WHERE trip_id = ?";
+        try (Connection conn = DatabaseConnector.connect(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, trip.getTripID());
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Trip> getAllTrips() {
+        String sql = "SELECT * FROM trips";
+        List<Trip> trips = new ArrayList<>();
+        try (Connection conn = DatabaseConnector.connect(); PreparedStatement stmt = conn.prepareStatement(sql) ) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+
+                String tripID = rs.getString("trip_id");
+                String origin = rs.getString("origin");
+                String destination = rs.getString("destination");
+
+                DatePicker departureTime = new DatePicker();
+                int busId = rs.getInt("bus_id");
+
+                Trip trip = new Trip();
+                trip.setTripID(tripID);
+                trip.setOrigin(origin);
+                trip.setDestination(destination);
+                trip.setDepartureTime(departureTime);
+                Bus bus =new Bus();
+                trip.setBus(bus);
+                trips.add(trip);
+            }
+            return trips;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Trip getTrip(String tripID) {
+        String sql = "SELECT * FROM trips WHERE trip_id = ?";
+            Trip trip = null;
+            try (Connection connection = DatabaseConnector.connect(); PreparedStatement stmt = connection.prepareStatement(sql) ) {
+                stmt.setString(1, tripID);
+                ResultSet rs = stmt.executeQuery();
+                if (rs.next()) {
+                    trip = new Trip();
+                    trip.setTripID(tripID);
+                    trip.setOrigin(rs.getString("origin"));
+                    trip.setDestination(rs.getString("destination"));
+                    DatePicker departureTime = new DatePicker();
+                    trip.setDepartureTime(departureTime);
+                    trip.setTime(LocalDateTime.parse(rs.getString("time")));
+                    int busId = rs.getInt("bus_id");
+                    Bus bus = new Bus();
+                    trip.setBus(bus);
+                }
+                return trip;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
     }
 }
---
+++ src/main/java/org/example/managers/TripManager.java
@@ -0,0 +1,33 @@
+package org.example.managers;
+
+import org.example.helper.dao.TripDAO;
+import org.example.models.Trip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripManager {
+    private static TripManager instance;
+    private List<Trip> trips;
+    private int tripIDcounter;
+    private TripDAO tripDAO = new TripDAO();
+
+    private TripManager getInstance() {
+        if (instance == null) {
+            instance = new TripManager();
+        }
+        return instance;
+    }
+
+    //database'e Trip eklemek için kullanılır return type
+    public boolean createTrip(Trip trip) {return tripDAO.insertTrip(trip);}
+
+    // verilen trip nesnesini databaseden kaldırır eğer başarılıysa true döner.
+    public boolean deleteTrip(Trip trip){return tripDAO.deleteTrip(trip);}
+
+    //Databasedeki tüm trip nesnelerini liste olarak döner.
+    public List<Trip> getAllTrips(){return tripDAO.getAllTrips();}
+
+    //ID'ye göre databaseden tek bir trip nesnesini çeker.
+    public Trip getTripById(String id){return tripDAO.getTrip(id);}
+}
